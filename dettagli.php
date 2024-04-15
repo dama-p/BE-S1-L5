1d39687f36181da3bb7854ec22a7370c957c58ef
Damaride Putignano <damarideputignano@gmail.com>
2024-04-15 12:32:15 +0200

    Added buttons on details

--- dettagli.php
+++ dettagli.php
@@ -15,6 +15,10 @@ include __DIR__ . '/includes/initial.php';
     <h1><?= $row['titolo'] ?></h1>
     <h2><?= $row['autore'] ?></h2>
     <p><?= $row['anno_pubblicazione']  . " " .  $row['genere'] ?></p>
+    <a href="/FSD%20IFOA/BE-S1-L5/dettagli.php?id=<?= $row['id'] ?>" class="btn btn-primary">Dettagli</a>
+    <a href="/FSD%20IFOA/BE-S1-L5/edit.php?id=<?= $row['id'] ?>" class="btn btn-warning">Edit</a>
+                <a href="/FSD%20IFOA/BE-S1-L5/elimina.php?id=<?= $row['id'] ?>" class="btn btn-danger">Elimina</a>
+
 
 
     <div>
